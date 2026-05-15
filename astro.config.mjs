1b816822260aa6db1f1db93c2b994393f027d34a
Tate Carsson <tatecarson@pm.me>
2026-05-15 13:48:06 -0500

    Redact contributor names for blind review

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://tatecarson.github.io",
-  base: "/drift-website-testing",
   server: {
     host: true,
     port: 4173
